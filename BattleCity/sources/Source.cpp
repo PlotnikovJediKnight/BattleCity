806b0f28f67f1cf6920861df5a074172869a2fb2
Vladislav Plotnikov <vadimovich74@gmail.com>
2022-12-26 22:32:07 +0300

    hi

--- BattleCity/sources/Source.cpp
+++ BattleCity/sources/Source.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
 	cout << "Hello, Field Message Branch!" << endl;
 	cout << "Hello, Field Message Branch! Commit 2" << endl;
+	cout << "Hello, Field Message Branch For Real! Commit 3" << endl;
 	cout << "Hello World!" << endl;
 	return 0;
 }
